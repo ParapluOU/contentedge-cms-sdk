882d93fc9d347d8d23ce70f6a650805d9d30df1f
Marios Tsirogiannis <barkasgreco@gmail.com>
2025-11-12 03:24:34 +0100

    Refactor CmsError constructor to handle cause option more cleanly

--- src/errors/CmsError.ts
+++ src/errors/CmsError.ts
@@ -2,13 +2,9 @@ export class CmsError extends Error {
     readonly status?: number;
     readonly data?: unknown;
     constructor(message: string, opts?: { status?: number; data?: unknown; cause?: unknown }) {
-        super(message);
+        super(message, opts?.cause ? { cause: opts.cause } : undefined);
         this.name = 'CmsError';
         this.status = opts?.status;
         this.data = opts?.data;
-        if (opts?.cause) {
-            // @ts-ignore
-            this.cause = opts.cause;
-        }
     }
 }
