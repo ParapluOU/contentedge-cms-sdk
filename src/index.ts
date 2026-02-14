31328c4bbb5845ea9bc80b2c9120214348f2c73e
Marios Tsirogiannis <barkasgreco@gmail.com>
2026-02-14 22:52:22 +0100

    Refactor index.ts to organize exports by category for improved clarity

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-export * from './errors/CmsError';
-export * from './auth/AuthProvider';
-export * from './auth/KeycloakClientCredentialsAuth';
+// Services
+export { apiClient, createApiClient, getApiClient, resetApiClient } from './services/apiClient';
+export * from './services/contentApi';
+export * from './services/contentFetchAll';
+
+// Queries (React Query integration - optional)
+export * from './queries/contentQueries';
+export * from './queries/queryKeys';
+export * from './queries/useContentQueries';
+
+// Types
 export * from './types/content';
+export * from './types/config';
+
+// Utils
 export * from './utils/assetUrl';
-export * from './CmsClient';
+export * from './utils/normalization';
+
+// Errors
+export * from './errors/CmsError';
+
+// Config
+export * from './config/sdkConfig';
